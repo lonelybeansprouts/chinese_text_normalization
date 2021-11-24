1d950d2af15aca11db8c4907361c7d28eee4fa27
dj <zhongdejian.wtt@bytedance.com>
2021-11-24 19:21:26 +0800

    update

--- thrax/src/cn_cpp/test.cc
+++ thrax/src/cn_cpp/test.cc
@@ -28,12 +28,39 @@ inline void MakeByteAcceptor(std::string &input,
 }
 
 
+template <class Arc>
+inline void MakeByteAcceptorXX(std::string &input,
+                          fst::MutableFst<Arc>* ofst) {
+    typedef typename Arc::StateId StateId;
+    typedef typename Arc::Weight Weight;
+
+    ofst->DeleteStates();
+    StateId cur_state = ofst->AddState();
+    ofst->SetStart(cur_state);
+    for (int i=0; i<input.size(); i++){
+        if (i>0){
+            StateId next_state = ofst->AddState();
+            ofst->AddArc(cur_state, Arc(0, 0, Weight::One(), next_state));
+            ofst->AddArc(cur_state, Arc('#', '#', -0.0000001, next_state));
+            cur_state = next_state;
+        }
+        unsigned char label = input.at(i);
+        StateId next_state = ofst->AddState();
+        Arc arc(label, label, Weight::One(), next_state);
+        ofst->AddArc(cur_state, arc);
+        cur_state = next_state;
+    }
+    ofst->SetFinal(cur_state, Weight::One());
+}
+
+
+
 
 
 int main(int args, char *argv[]){
     // because thrax fst use one-Byte-char as label
 
-    std::string input_str = "十月四日";
+    std::string input_str = "今天是十一月二十四日吗";
     std::string fst_path = "../cn/ITN.fst";
 
 
@@ -51,29 +78,29 @@ int main(int args, char *argv[]){
     fst::VectorFst<fst::StdArc> * output_cpy = new fst::VectorFst<fst::StdArc>(output);
 
     { 
-    fst::Project(&output, fst::PROJECT_OUTPUT);
-    // lattice to string
-    std::string tmp_str;
-    std::string output_str;
-    printer(output, &tmp_str);
-    for (unsigned char c: tmp_str) {
-        if (c != 0) output_str += c;
-    }
+        fst::Project(&output, fst::PROJECT_OUTPUT);
+        // lattice to string
+        std::string tmp_str;
+        std::string output_str;
+        printer(output, &tmp_str);
+        for (unsigned char c: tmp_str) {
+            if (c != 0) output_str += c;
+        }
 
-    std::cout<<output_str<<"\n";
+        std::cout<<output_str<<"\n";
     }
 
     { 
-    fst::Project(output_cpy, fst::PROJECT_INPUT);
-    // lattice to string
-    std::string tmp_str;
-    std::string output_str;
-    printer(*output_cpy, &tmp_str);
-    for (unsigned char c: tmp_str) {
-        if (c != 0) output_str += c;
-    }
+        fst::Project(output_cpy, fst::PROJECT_INPUT);
+        // lattice to string
+        std::string tmp_str;
+        std::string output_str;
+        printer(*output_cpy, &tmp_str);
+        for (unsigned char c: tmp_str) {
+            if (c != 0) output_str += c;
+        }
 
-    std::cout<<output_str<<"\n";
+        std::cout<<output_str<<"\n";
     }
     return 0;
 }
